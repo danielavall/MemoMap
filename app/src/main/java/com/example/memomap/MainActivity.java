b2d1359bf63babc4d7c78ec581429108dcae98f9
Teresa Naomi <teresanaomi10@gmail.com>
2025-07-15 15:47:06 +0700

    feat(nav): add bottom navigation and connect all fragments

---
+++ app/src/main/java/com/example/memomap/MainActivity.java
@@ -0,0 +1,47 @@
+package com.example.memomap;
+
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.fragment.app.Fragment;
+import androidx.fragment.app.FragmentManager;
+import androidx.fragment.app.FragmentTransaction;
+
+import android.os.Bundle;
+
+import com.example.memomap.databinding.ActivityMainBinding;
+
+public class MainActivity extends AppCompatActivity {
+
+    ActivityMainBinding binding;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        binding = ActivityMainBinding.inflate(getLayoutInflater());
+        setContentView(binding.getRoot());
+
+        replaceFragment(new HomeFragment());
+        binding.bottomNavigationView.setBackground(null);
+
+        binding.bottomNavigationView.setOnItemSelectedListener(item -> {
+            int id = item.getItemId();
+            if (id == R.id.home) {
+                replaceFragment(new HomeFragment());
+            } else if (id == R.id.goal) {
+                replaceFragment(new GoalFragment());
+            } else if (id == R.id.recap) {
+                replaceFragment(new RecapFragment());
+            } else if (id == R.id.suggest) {
+                replaceFragment(new SuggestFragment());
+            }
+            return true;
+        });
+
+    }
+
+    private void replaceFragment(Fragment fragment) {
+        FragmentManager fragmentManager = getSupportFragmentManager();
+        FragmentTransaction fragmentTransaction = fragmentManager.beginTransaction();
+        fragmentTransaction.replace(R.id.frame_layout, fragment);
+        fragmentTransaction.commit();
+    }
+}
